a1e8e12b6d9f8b3395aefbc3a8297a664e30ef98
Kalyani T <47531677+Kalyani2019@users.noreply.github.com>
2022-02-03 11:11:45 -0500

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWSLambda1
+{
+    public class Student
+    {
+        public string emailid { get; set; }
+        public string name { get; set; }
+    }
+}
